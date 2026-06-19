0aece34d43fc302027ead8ec0fa38f299eab2aac
KDSealock <158773839+Arrhythmiagamer23@users.noreply.github.com>
2026-06-19 11:55:44 -0400

    Add files via upload

--- include/main.hpp
+++ include/main.hpp
@@ -436,7 +436,7 @@ namespace GameObjectsFactory {
     }
 
     /**
-     * Helper function to create a configuration for custom ring
+     * Helper function to create a configuration for custom dash ring
      *
      * @param objectID - Your custom object ID
      * @param spriteFrame - Sprite frame for the object
@@ -463,6 +463,38 @@ namespace GameObjectsFactory {
         auto config = createObjectConfig(objectID, spriteFrame, cllbk, refObjectID);
         config->m_createTabBar = 5; //portal-tab
         config->m_tabBarInsertIndex = 16;
+        config->m_activatedByPlayer = activatedByPlayer;
+        return config;
+    }
+
+    /**
+     * Helper function to create a configuration for custom pad
+     *
+     * @param objectID - Your custom object ID
+     * @param spriteFrame - Sprite frame for the object
+     * @param setupCallback - Function called on setup
+     * @param activatedByPlayer - Function called when activates
+     * @param spriteFrameDetail - Detail frame (only if refer id is 1594!)
+     * @param refObjectID - Refer obj
+     */
+    inline GameObjectConfig* createPadConfig(
+        int objectID,
+        const std::string& spriteFrame = "",
+        std::function<void(EnhancedGameObject*, PlayerObject*)> activatedByPlayer = nullptr,
+        std::function<void(GameObject*)> setupCallback = nullptr,
+        const std::string& spriteFrameDetail = "emptyGlow.png",
+        int refObjectID = 140
+    ) {
+        auto cllbk = [=](GameObject* obj) mutable {
+            if (spriteFrameDetail.size()) if (auto a = obj->m_colorSprite) a->initWithSpriteFrameName(
+                spriteFrameDetail.c_str()
+            );
+
+            if (setupCallback) setupCallback(obj);
+            };
+        auto config = createObjectConfig(objectID, spriteFrame, cllbk, refObjectID);
+        config->m_createTabBar = 5; //portal-tab
+        config->m_tabBarInsertIndex = 4;
         config->m_activatedByPlayer = activatedByPlayer;
         return config;
     }
